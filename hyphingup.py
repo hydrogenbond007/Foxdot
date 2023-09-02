a59932e17c8247f4ab09941b6fa6754b397e4188
Madhav Goyal <88841339+hydrogenbond007@users.noreply.github.com>
2023-09-03 00:30:10 +0530

    Add files via upload

---
+++ hyphingup.py
@@ -0,0 +1,16 @@
+p1 >> pads([0,1,2,3], dur=[1/4,3/4], sus=1, vib=4, scale=Scale.minor)
+
+d1 >> play("xw-o{-[--]o[-o]}")
+bd >> play("wvwvs")
+
+
+hh>> play("e")
+
+
+sn >>play("kqs")
+
+
+# Clock.clear()
+print(Sample)
+
+#d1 >> play("@")
